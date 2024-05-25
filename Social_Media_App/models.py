d1a2336534ad3cf79407ea5f2e5accc02a06a170
ASHOK KUMAR M <276ashokkumar@gmail.com>
2024-05-25 10:12:09 +0530

    Snake code added

--- Social_Media_App/models.py
+++ Social_Media_App/models.py
@@ -3,8 +3,11 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 
 # Create your models here.
+
+
 class CustomUser(AbstractUser):
-    email = models.EmailField(max_length=100, unique=True, null=False, blank=False)
+    email = models.EmailField(
+        max_length=100, unique=True, null=False, blank=False)
     mobile_regex = RegexValidator(regex=r"^[6-9]\d{9}$")
     mobile_number = models.CharField(
         validators=[mobile_regex], max_length=10, unique=True, null=False, blank=False
@@ -14,10 +17,13 @@ class CustomUser(AbstractUser):
     )
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    following = models.ManyToManyField(
+        'self', symmetrical=False, related_name='followers')
 
     def __str__(self):
         return self.username
+
+
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
