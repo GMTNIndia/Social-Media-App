5c9fe748ab8df911c8bbc7b174fa258d14cf5027
= <=>
2024-06-03 13:38:30 +0530

    add some features

--- Social_Media/settings.py
+++ Social_Media/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -92,6 +93,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "Social_Media.wsgi.application"
+ASGI_APPLICATION = "Social_Media.asgi.application"
 
 
 # Database
--- Social_Media_App/consumers.py
+++ Social_Media_App/consumers.py
@@ -2,6 +2,7 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.contrib.auth import get_user_model
+from .models import Message, Notification
 
 User = get_user_model()
 
@@ -35,3 +36,74 @@ class NotificationConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             "notification": notification
         }))
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope["user"]
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+
+        if self.user.is_authenticated:
+            # Join room group
+            await self.channel_layer.group_add(
+                self.room_group_name,
+                self.channel_name
+            )
+
+            await self.accept()
+        else:
+            await self.close()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        if self.user.is_authenticated:
+            # Get the receiver from the room name
+            receiver_username = self.room_name.replace(self.user.username, "").replace("_", "")
+            receiver = await self.get_user_by_username(receiver_username)
+            
+            if receiver:
+                # Create the message in the database
+                await self.create_message(self.user, receiver, message)
+
+                # Send message to room group
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        'type': 'chat_message',
+                        'message': message,
+                        'sender': self.user.username,
+                    }
+                )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event['message']
+        sender = event['sender']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'sender': sender,
+        }))
+
+    @database_sync_to_async
+    def get_user_by_username(self, username):
+        try:
+            return User.objects.get(username=username)
+        except User.DoesNotExist:
+            return None
+
+    @database_sync_to_async
+    def create_message(self, sender, receiver, message):
+        return Message.objects.create(sender=sender, receiver=receiver, message=message)
