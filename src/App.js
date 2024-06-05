3e60396c0525ec13119cb516fc02ca3c4707cab4
= <=>
2024-06-05 10:56:34 +0530

    Home page changed

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <Navbar/>
     <Routes>
       
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
         <Route path="*" element={<Nopage/>} />
--- src/auth/Home.js
+++ src/auth/Home.js
@@ -1,21 +1,24 @@
 
 import React from 'react';
-import Sidebar from '../components/Sidebar';
-import MainContent from '../components/MainContent';
-import RightSidebar from '../components/RightSidebar';
+// import Sidebar from '../components/Sidebar';
+// import MainContent from '../components/MainContent';
+// import RightSidebar from '../components/RightSidebar';
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    // <div className="flex flex-col min-h-screen">
     
-      <div className="flex flex-1 flex-col md:flex-row">
-      <RightSidebar className="order-1 md:order-3 w-full md:w-1/4 p-4" />
-      <MainContent className="order-2 md:order-2 w-full md:w-1/2 p-4" />
-      <Sidebar className="order-3 md:order-1 w-full md:w-1/4 p-4" />
+    //   <div className="flex flex-1 flex-col md:flex-row">
+    //   <RightSidebar className="order-1 md:order-3 w-full md:w-1/4 p-4" />
+    //   <MainContent className="order-2 md:order-2 w-full md:w-1/2 p-4" />
+    //   <Sidebar className="order-3 md:order-1 w-full md:w-1/4 p-4" />
         
        
-      </div>
-    </div>
+    //   </div>
+    // </div>
+    <>
+      <h1>Home page will added</h1>
+    </>
   );
 }
 
