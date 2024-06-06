816e3863142ee8779cf94e037fc2ec0ab571f183
= <=>
2024-06-06 10:38:38 +0530

    video added in post

--- Social_Media_App/models.py
+++ Social_Media_App/models.py
@@ -31,7 +31,7 @@ class Post(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     content = models.TextField()
-    image = models.ImageField(upload_to="post_images/", null=True, blank=True)
+    image = models.FileField(upload_to="post_images/", null=True, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
