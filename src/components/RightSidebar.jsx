a63045e579dd103302008e5df45715681448a4a5
= <=>
2024-06-06 16:26:37 +0530

    Rightsidebar added

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -416,7 +416,7 @@ function Sidebar() {
                       type="button"
                       onClick={handleSubmit}
                     >
-                      Save
+                      Upload
                     </button>
                   </div>
                 </div>
